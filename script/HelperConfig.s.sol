45292d223a7e4be34a2c4adf94aa39c95d21a59c
yebology <yobelnathaniel12@gmail.com>
2023-12-27 00:31:07 +0700

    make test

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier : MIT
+
+pragma solidity ^0.8.18;
+
+import { Script } from "forge-std/Script.sol";
+import { VRFCoordinatorV2Mock } from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+
+contract HelperConfig is Script {
+    
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint64 subscriptionId;
+        uint32 callbackGasLimit;
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        }
+        else {
+            activeNetworkConfig = getorCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+
+        return NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+            gasLane: 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15,
+            subscriptionId: 0,
+            callbackGasLimit: 500000
+        });
+
+    }
+
+    function getorCreateAnvilEthConfig() public returns(NetworkConfig memory) {
+
+        if (activeNetworkConfig.vrfCoordinator != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        uint96 baseFee = 0.25 ether;
+        uint96 gasPriceLink = 1e9;
+
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vrfCoordinatorV2Mock = new VRFCoordinatorV2Mock(baseFee, gasPriceLink);
+        vm.stopBroadcast();
+
+        return NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: address(vrfCoordinatorV2Mock),
+            gasLane: 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15,
+            subscriptionId: 0,
+            callbackGasLimit: 500000
+        });
+
+    }
+}
